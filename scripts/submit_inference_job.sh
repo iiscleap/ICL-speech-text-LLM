d001cd781dcc1c13ae5d908a98b272e0ea987e3c
magnieet <neeraj13788@gmail.com>
2025-04-18 00:00:03 +0530

    Auto-commit: 2025-04-18 00:00:01

--- scripts/submit_inference_job.sh
+++ scripts/submit_inference_job.sh
@@ -5,7 +5,7 @@ model_type="salmonn"  # Options: "salmonn" or "qwen2"
 dataset_type="meld_emotion,voxpopuli,hvb,voxceleb"  # Options: "voxceleb", "hvb", "meld_emotion" "voxpopuli", etc., sqa, vp_nel
 input_mode="speech_only"  # Options: "speech_only", "text_only", "speech_and_text"
 fewshot_mode="text"  # Options: "text" or "speech"
-num_examples=0
+num_examples=2
 batch_size=1
 
 debug_samples=0  # Add debug_samples parameter (0 = use all samples)
@@ -15,8 +15,8 @@ randomize_swap=false  # Set to true to randomize swap configurations
 
 # Node configuration
 queue_name="med.q"      # Queue to submit job to (gpu.q, med.q, etc.)
-hostname="compute-0-7"  # Hostname to run on
-cuda_device=1         # CUDA device to use
+hostname="compute-0-9"  # Hostname to run on
+cuda_device=2         # CUDA device to use
 hold_job_id=""          # Job ID to wait for (empty = don't wait)
 
 
@@ -60,7 +60,7 @@ peft_model_path=""
 # peft_model_path="/data2/neeraja/neeraja/results/model_ICL/trained_models/2503_1535_ft_5ex_20e8b_salmonn_text_only_text_voxceleb_swap-hvb_swap/checkpoints/epoch_10_loss_0.1935/model.pt"
 
 
-peft_model_path="/data2/neeraja/neeraja/results/model_ICL/trained_models/0204_1839_ft_5ex_20e8b_salmonn_speech_only_text_voxceleb_swap-hvb_swap_SS_/checkpoints/epoch_10_loss_0.0324/model.pt"
+# peft_model_path="/data2/neeraja/neeraja/results/model_ICL/trained_models/0204_1839_ft_5ex_20e8b_salmonn_speech_only_text_voxceleb_swap-hvb_swap_SS_/checkpoints/epoch_10_loss_0.0324/model.pt"
 
 # peft_model_path="/data2/neeraja/neeraja/results/model_ICL/trained_models/2503_1539_ft_5ex_20e8b_salmonn_speech_only_text_voxceleb_swap-hvb_swap_ss/checkpoints/epoch_10_loss_0.0063/model.pt"
 
@@ -77,6 +77,12 @@ peft_model_path="/data2/neeraja/neeraja/results/model_ICL/trained_models/0204_18
 # peft_model_path="/data2/neeraja/neeraja/results/model_ICL/trained_models/0904_1028_ft_5ex_20e8b_salmonn_speech_only_text_meld_emotion-voxpopuli/checkpoints/epoch_10_loss_0.0199/model.pt"
 # peft_model_path="/data2/neeraja/neeraja/results/model_ICL/trained_models/0904_1108_ft_5ex_20e8b_salmonn_speech_only_text_meld_emotion_greek-voxpopuli_greek/checkpoints/epoch_10_loss_0.0192/model.pt"
 # peft_model_path="/data2/neeraja/neeraja/results/model_ICL/trained_models/0904_1108_ft_5ex_20e8b_salmonn_speech_only_text_meld_emotion_greek-voxpopuli_greek/final_model.pt"
+
+
+
+# peft_model_path="/data2/neeraja/neeraja/results/model_ICL/trained_models/1604_0129_ft_5ex_20e8b_salmonn_speech_only_text_meld_emotion_greek-voxpopuli_greek/checkpoints/epoch_15_loss_0.0029/model.pt"
+peft_model_path="/data2/neeraja/neeraja/results/model_ICL/trained_models/1604_0113_ft_5ex_20e8b_salmonn_speech_only_text_meld_emotion_swap-voxpopuli_swap/checkpoints/epoch_15_loss_0.0082/model.pt"
+# peft_model_path="/data2/neeraja/neeraja/results/model_ICL/trained_models/1604_0121_ft_5ex_20e8b_salmonn_speech_only_text_meld_emotion_swap-voxpopuli_swap_nosymbol/checkpoints/epoch_15_loss_0.0078/model.pt"
 
 # Clean dataset type for file names and Python
 if [[ $dataset_type == *","* ]]; then
